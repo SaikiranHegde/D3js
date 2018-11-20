8c38d9282a420c7db13198f17b6e74c6201dca80
SaikiranHegde <saikiranhegde0793@gmail.com>
2018-11-20 11:17:35 +0530

    Added Transition & Animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,26 @@
 const barData = [];
+let color;
 
-for (let i = 0; i < 100; i++) {
-    barData.push(Math.random() * 30);
+for (let i = 0; i < 40; i++) {
+    barData.push(Math.random() * 30 + 5);
 }
 
 const height = 400, width = 600, barWidth = 50, barOffset = 5;
 
 let yScale = d3.scaleLinear()
-                .domain([0, d3.max(barData)])
-                .range([0, height]);
+    .domain([0, d3.max(barData)])
+    .range([0, height]);
 
 let xScale = d3.scaleBand()
-                .domain(barData)
-                .paddingInner(.3)
-                .range([0, width]);
+    .domain(barData)
+    .paddingInner(.3)
+    .range([0, width]);
 
 let colors = d3.scaleLinear()
-                .domain([0, barData.length * .33, barData.length * .66, barData.length])
-                .range(['#FFB832', '#C61C6F', '#268BD2', '#85992C']);
+    .domain([0, barData.length * .33, barData.length * .66, barData.length])
+    .range(['#FFB832', '#C61C6F', '#268BD2', '#85992C']);
 
-d3.select('#viz')
+let myChart = d3.select('#viz')
     .append('svg')
     .attr('width', width)
     .attr('height', height)
@@ -33,15 +34,11 @@ d3.select('#viz')
     .attr('width', (d) => {
         return xScale.bandwidth();
     })
-    .attr('height', (d) => {
-        return yScale(d);
-    })
+    .attr('height', 0)
     .attr('x', (d) => {
         return xScale(d);
     })
-    .attr('y', (d) => {
-        return height - yScale(d);
-    })
+    .attr('y', height)
     .on('mouseover', function(d) {
         color = this.style.fill;
         d3.select(this)
@@ -50,4 +47,18 @@ d3.select('#viz')
     .on('mouseout', function(d) {
         d3.select(this)
             .style('fill', color);
+    });
+
+
+myChart.transition()
+    .attr('height', (d) => {
+        return yScale(d);
     })
+    .attr('y', (d) => {
+        return height - yScale(d);
+    })
+    .delay((d, i) => {
+        return i * 20;
+    })
+    .duration(1000)
+    .ease(d3.easeBounceOut)
