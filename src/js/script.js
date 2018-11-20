5fe8dcdbfaff6ac99481425b2008fdb0a3c39768
SaikiranHegde <saikiranhegde0793@gmail.com>
2018-11-20 18:45:33 +0530

    Added Tooltip

--- src/js/script.js
+++ src/js/script.js
@@ -20,6 +20,13 @@ let colors = d3.scaleLinear()
     .domain([0, barData.length * .33, barData.length * .66, barData.length])
     .range(['#FFB832', '#C61C6F', '#268BD2', '#85992C']);
 
+let tooltip = d3.select('body')
+    .append('div')
+    .style('position', 'absolute')
+    .style('padding', '0 10px')
+    .style('background', 'white')
+    .style('opacity', 0);
+
 let myChart = d3.select('#viz')
     .append('svg')
     .attr('width', width)
@@ -40,11 +47,23 @@ let myChart = d3.select('#viz')
     })
     .attr('y', height)
     .on('mouseover', function(d) {
+        tooltip.transition()
+            .duration(200)
+            .style('opacity', .9);
+
+        tooltip.html(d)
+            .style('left', (d3.event.pageX - 35) + 'px')
+            .style('top', (d3.event.pageY - 30) + 'px' );
+        
         color = this.style.fill;
         d3.select(this)
             .style('fill', 'yellow');
     })
     .on('mouseout', function(d) {
+        tooltip.transition()
+            .duration(200)
+            .style('opacity', 0);
+
         d3.select(this)
             .style('fill', color);
     });
